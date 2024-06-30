27d06e49d2ff9f0319c4a4298de118d8f0c35ec0
danieljvb <jonatan_dvb@hotmail.com>
2024-06-29 23:11:03 -0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <div className='bg-background'>
       
       
-      <BrowserRouter basename='/santaclarapalsoccer/'>
+      <BrowserRouter >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<Aboutus />} />
